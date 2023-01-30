c5dcdebdb28439668a3e7adefe044073c13076b1
m2nja201 <m2nja201@gmail.com>
2023-01-31 03:17:36 +0900

    chore : repository 파일 생성, BaseEntity 수정

--- backend/src/main/java/com/inti/nosily_coach/domain/Exercise/repository/ExerciseRepositoryCustomImpl.java
+++ backend/src/main/java/com/inti/nosily_coach/domain/Exercise/repository/ExerciseRepositoryCustomImpl.java
@@ -1,7 +1,4 @@
 package com.inti.nosily_coach.domain.Exercise.repository;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
-
 public class ExerciseRepositoryCustomImpl implements ExerciseRepositoryCustom {
-    JPAQueryFactory queryFactory;
 }
--- backend/src/main/java/com/inti/nosily_coach/domain/common/BaseEntity.java
+++ backend/src/main/java/com/inti/nosily_coach/domain/common/BaseEntity.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
-@EntityListeners((AuditingEntityListener.class))
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
