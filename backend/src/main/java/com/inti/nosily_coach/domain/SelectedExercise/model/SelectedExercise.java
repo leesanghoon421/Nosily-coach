c10f3e81dc2e8d01639f55c687af49c966ec0095
m2nja201 <m2nja201@gmail.com>
2023-01-31 03:00:52 +0900

    fix : SelectedExercise 칼럼 추가

--- backend/src/main/java/com/inti/nosily_coach/domain/SelectedExercise/model/SelectedExercise.java
+++ backend/src/main/java/com/inti/nosily_coach/domain/SelectedExercise/model/SelectedExercise.java
@@ -3,14 +3,14 @@ package com.inti.nosily_coach.domain.SelectedExercise.model;
 import com.inti.nosily_coach.domain.Exercise.model.Exercise;
 import com.inti.nosily_coach.domain.ExerciseRecord.model.ExerciseRecord;
 import com.inti.nosily_coach.domain.common.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+import java.util.Timer;
 
 @Entity
 @Getter
@@ -24,16 +24,31 @@ public class SelectedExercise extends BaseEntity {
     @JoinColumn(name = "exercise_id")
     private Exercise exercise;
 
+    @Column
+    private LocalTime time; // 운동시간
+
+    @Column
+    private Long count; // 1회 횟수
+
+    @Column
+    private Long set; // set 횟수
+
     @Builder(access = AccessLevel.PRIVATE)
-    private SelectedExercise(ExerciseRecord exerciseRecord, Exercise exercise) {
+    private SelectedExercise(ExerciseRecord exerciseRecord, Exercise exercise, LocalTime time, Long count, Long set) {
         this.exerciseRecord = exerciseRecord;
         this.exercise = exercise;
+        this.time = time;
+        this.count = count;
+        this.set = set;
     }
 
-    public static SelectedExercise newSelectedExercise(ExerciseRecord exerciseRecord, Exercise exercise) {
+    public static SelectedExercise newSelectedExercise(ExerciseRecord exerciseRecord, Exercise exercise, LocalTime time, Long count, Long set) {
         return SelectedExercise.builder()
                 .exerciseRecord(exerciseRecord)
                 .exercise(exercise)
+                .time(time)
+                .count(count)
+                .set(set)
                 .build();
     }
 }
