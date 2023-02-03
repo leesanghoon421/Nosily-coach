b92ec36008fe91c6d79a9dd25ac3ada54700967d
m2nja201 <m2nja201@gmail.com>
2023-02-03 17:45:28 +0900

    chore : queryFactory 객체 생성

--- backend/src/main/java/com/inti/nosily_coach/domain/Exercise/repository/ExerciseRepositoryCustomImpl.java
+++ backend/src/main/java/com/inti/nosily_coach/domain/Exercise/repository/ExerciseRepositoryCustomImpl.java
@@ -1,4 +1,10 @@
 package com.inti.nosily_coach.domain.Exercise.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class ExerciseRepositoryCustomImpl implements ExerciseRepositoryCustom {
+    private final JPAQueryFactory queryFactory;
+
 }
