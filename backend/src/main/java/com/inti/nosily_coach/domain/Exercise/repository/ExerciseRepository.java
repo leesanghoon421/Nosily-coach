f67e3f77e15fe0fbb5ae83b28f35d0e852c700c7
m2nja201 <m2nja201@gmail.com>
2023-02-03 17:45:59 +0900

    feat : 운동법 전체 조회 - 기본 사용 메서드 호출

--- backend/src/main/java/com/inti/nosily_coach/domain/Exercise/repository/ExerciseRepository.java
+++ backend/src/main/java/com/inti/nosily_coach/domain/Exercise/repository/ExerciseRepository.java
@@ -1,7 +1,11 @@
 package com.inti.nosily_coach.domain.Exercise.repository;
 
 import com.inti.nosily_coach.domain.Exercise.model.Exercise;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.awt.print.Pageable;
+
 public interface ExerciseRepository extends JpaRepository<Exercise, Long>, ExerciseRepositoryCustom {
+    Page<Exercise> findAll(Pageable pageable);
 }
