2f88d5375339cca250214daa49936f19bb2913df
m2nja201 <m2nja201@gmail.com>
2023-02-04 20:23:23 +0900

    fix : controller에 responseBody 추가

--- backend/src/main/java/com/inti/nosily_coach/domain/Exercise/controller/ExerciseController.java
+++ backend/src/main/java/com/inti/nosily_coach/domain/Exercise/controller/ExerciseController.java
@@ -6,6 +6,7 @@ import com.inti.nosily_coach.domain.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.data.domain.Pageable;
@@ -18,6 +19,7 @@ public class ExerciseController {
 
     // # 운동법 전체 조회
     @GetMapping("/api/exercises")
+    @ResponseBody
     public ApiResponse<List<GetExerciseResponse>> getAllExercises(@PageableDefault Pageable pageable) {
         List<GetExerciseResponse> getExerciseResponses = exerciseService.getAllExercises(pageable);
         return ApiResponse.success(getExerciseResponses);
