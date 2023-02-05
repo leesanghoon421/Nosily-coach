3547d21ce52362f086f8e917dee90ca20f384b51
m2nja201 <m2nja201@gmail.com>
2023-02-05 15:19:50 +0900

    fix : 운동 기록 작성 - 오늘 작성된 게시물 수 반환 쿼리 작성

--- backend/src/main/java/com/inti/nosily_coach/domain/ExerciseRecord/repository/ExerciseRecordRepositoryCustom.java
+++ backend/src/main/java/com/inti/nosily_coach/domain/ExerciseRecord/repository/ExerciseRecordRepositoryCustom.java
@@ -2,6 +2,9 @@ package com.inti.nosily_coach.domain.ExerciseRecord.repository;
 
 import com.inti.nosily_coach.domain.ExerciseRecord.model.ExerciseRecord;
 
+import java.time.LocalDate;
+
 public interface ExerciseRecordRepositoryCustom {
     ExerciseRecord findExerciseRecordByRecordId(Long memberId, Long recordId);
+    Long getNumOfTodayRecord(Long memberId, LocalDate date);
 }
--- backend/src/main/java/com/inti/nosily_coach/domain/ExerciseRecord/repository/ExerciseRecordRepositoryCustomImpl.java
+++ backend/src/main/java/com/inti/nosily_coach/domain/ExerciseRecord/repository/ExerciseRecordRepositoryCustomImpl.java
@@ -1,14 +1,21 @@
 package com.inti.nosily_coach.domain.ExerciseRecord.repository;
 
 import com.inti.nosily_coach.domain.ExerciseRecord.model.ExerciseRecord;
+import com.querydsl.core.types.Ops;
+import com.querydsl.core.types.dsl.NumberOperation;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDate;
+
 import static com.inti.nosily_coach.domain.ExerciseRecord.model.QExerciseRecord.exerciseRecord;
+import static com.querydsl.core.types.dsl.Expressions.numberOperation;
+
 @RequiredArgsConstructor
 public class ExerciseRecordRepositoryCustomImpl implements ExerciseRecordRepositoryCustom{
     private final JPAQueryFactory queryFactory;
 
+    // # recordId로 ExerciseRecord 불러오기
     @Override
     public ExerciseRecord findExerciseRecordByRecordId(Long memberId, Long recordId) {
         return queryFactory.selectFrom(exerciseRecord)
@@ -19,5 +26,22 @@ public class ExerciseRecordRepositoryCustomImpl implements ExerciseRecordReposit
                         exerciseRecord.id.eq(recordId)
                         )
                 .fetchOne();
+    }
+
+    // # 오늘 작성된 게 있는지
+    @Override
+    public Long getNumOfTodayRecord(Long memberId, LocalDate date) {
+        NumberOperation<Integer> toYear = numberOperation(Integer.class, Ops.DateTimeOps.YEAR, exerciseRecord.createdAt);
+        NumberOperation<Integer> toMonth = numberOperation(Integer.class, Ops.DateTimeOps.MONTH, exerciseRecord.createdAt);
+        NumberOperation<Integer> toDay = numberOperation(Integer.class, Ops.DateTimeOps.DAY_OF_MONTH, exerciseRecord.createdAt);
+        return queryFactory.select(exerciseRecord.count())
+                .where(
+                        // memberId에 해당되는 record인지 확인
+                        exerciseRecord.member.id.eq(memberId),
+                        // date에 맞는 record 개수
+                        toYear.eq(date.getYear()),
+                        toMonth.eq(date.getMonthValue()),
+                        toDay.eq(date.getDayOfMonth())
+                ).fetchOne();
     }
 }
