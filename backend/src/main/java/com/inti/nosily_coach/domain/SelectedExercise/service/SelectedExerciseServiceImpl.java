3a6fd98fca94d69f926b3823ce0df6499cd522be
m2nja201 <m2nja201@gmail.com>
2023-02-06 21:29:28 +0900

    Feat : 운동 선택 작성 기능 추가, DTO로 출력될 수 있도록 로직 추가

--- backend/src/main/java/com/inti/nosily_coach/domain/SelectedExercise/service/SelectedExerciseServiceImpl.java
+++ backend/src/main/java/com/inti/nosily_coach/domain/SelectedExercise/service/SelectedExerciseServiceImpl.java
@@ -1,25 +1,37 @@
 package com.inti.nosily_coach.domain.SelectedExercise.service;
 
 import com.inti.nosily_coach.domain.Exercise.model.Exercise;
+import com.inti.nosily_coach.domain.Exercise.repository.ExerciseRepository;
 import com.inti.nosily_coach.domain.ExerciseRecord.model.ExerciseRecord;
+import com.inti.nosily_coach.domain.ExerciseRecord.repository.ExerciseRecordRepository;
 import com.inti.nosily_coach.domain.SelectedExercise.model.SelectedExercise;
+import com.inti.nosily_coach.domain.SelectedExercise.model.dto.CreateSelectedExerciseRequest;
+import com.inti.nosily_coach.domain.SelectedExercise.model.dto.GetSelectedExerciseResponse;
 import com.inti.nosily_coach.domain.SelectedExercise.repository.SelectedExerciseRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class SelectedExerciseServiceImpl implements SelectedExerciseService{
     private final SelectedExerciseRepository selectedExerciseRepository;
+    private final ExerciseRepository exerciseRepository;
+    private final ExerciseRecordRepository exerciseRecordRepository;
 
     // 매핑
     @Override
     @Transactional
-    public void createSelectedExercise(Exercise exercise, ExerciseRecord exerciseRecord, LocalTime times, int counts, int setCnt) {
-        selectedExerciseRepository.save(SelectedExercise.newSelectedExercise(exerciseRecord, exercise, times, counts, setCnt));
+    public void createSelectedExercise(Long memberId, Long exerciseId, LocalDate localDate, CreateSelectedExerciseRequest request) {
+        Exercise exercise = exerciseRepository.findById(exerciseId).orElseThrow(()->new RuntimeException("해당되는 운동법이 없습니다."));
+        ExerciseRecord exerciseRecord = exerciseRecordRepository.findByDate(memberId, localDate);
+        selectedExerciseRepository.save(SelectedExercise.newSelectedExercise(exerciseRecord, exercise,
+                request.getTimes(), request.getCounts(), request.getSetCnt()));
     }
 
     // 매핑 수정
@@ -34,5 +46,14 @@ public class SelectedExerciseServiceImpl implements SelectedExerciseService{
     @Transactional
     public void deleteSelectedExercise(SelectedExercise selectedExercise) {
         selectedExerciseRepository.delete(selectedExercise);
+    }
+
+    // DTO로 변환하여 출력
+    @Override
+    @Transactional
+    public List<GetSelectedExerciseResponse> getSelectedExercise(List<SelectedExercise> selectedExercises) {
+        return selectedExercises.stream().map(
+                exercise -> GetSelectedExerciseResponse.of(exercise.getExercise().getName(),
+                        exercise.getCounts(), exercise.getSetCnt())).collect(Collectors.toList());
     }
 }
