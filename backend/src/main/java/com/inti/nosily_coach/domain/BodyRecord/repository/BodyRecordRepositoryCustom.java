0bd0a8f7921073f491d6984e7238ec75e2df978e
m2nja201 <m2nja201@gmail.com>
2023-02-06 15:39:06 +0900

    Feat : 몸기록 조회 - 전체, 날짜별 쿼리 작성

--- backend/src/main/java/com/inti/nosily_coach/domain/BodyRecord/repository/BodyRecordRepositoryCustom.java
+++ backend/src/main/java/com/inti/nosily_coach/domain/BodyRecord/repository/BodyRecordRepositoryCustom.java
@@ -1,4 +1,12 @@
 package com.inti.nosily_coach.domain.BodyRecord.repository;
 
+import com.inti.nosily_coach.domain.BodyRecord.model.BodyRecord;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface BodyRecordRepositoryCustom {
+    List<BodyRecord> findAllByMemberId(Long memberId, Pageable pageable);
+    BodyRecord findByDate(Long memberId, LocalDate localDate);
 }
