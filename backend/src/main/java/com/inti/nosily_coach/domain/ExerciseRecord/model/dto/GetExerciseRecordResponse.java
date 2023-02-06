1f025296f423e9fa8ff607e08cc01a8036ed1155
m2nja201 <m2nja201@gmail.com>
2023-02-06 21:26:13 +0900

    Fix : selectedExercise DTO로 내보내도록 수정

--- backend/src/main/java/com/inti/nosily_coach/domain/ExerciseRecord/model/dto/GetExerciseRecordResponse.java
+++ backend/src/main/java/com/inti/nosily_coach/domain/ExerciseRecord/model/dto/GetExerciseRecordResponse.java
@@ -1,6 +1,6 @@
 package com.inti.nosily_coach.domain.ExerciseRecord.model.dto;
 
-import com.inti.nosily_coach.domain.SelectedExercise.model.SelectedExercise;
+import com.inti.nosily_coach.domain.SelectedExercise.model.dto.GetSelectedExerciseResponse;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,11 +13,11 @@ import java.util.List;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class GetExerciseRecordResponse {
     private Long recordId;
-    private List<SelectedExercise> selectedExercises;
+    private List<GetSelectedExerciseResponse> selectedExercises;
     private String memo;
     private String createdAt;
 
-    public static GetExerciseRecordResponse of(Long recordId, List<SelectedExercise> selectedExercises,
+    public static GetExerciseRecordResponse of(Long recordId, List<GetSelectedExerciseResponse> selectedExercises,
                                                String memo, String createdAt) {
         return new GetExerciseRecordResponse(recordId, selectedExercises, memo, createdAt);
     }
