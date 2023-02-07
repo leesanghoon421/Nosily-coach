cbd8c58c7ebd16832e014f39cc23476beafcb4aa
Yeonhan Jung <yt4307@gmail.com>
2023-02-07 21:21:11 +0900

    [Style] 홈 화면 디자인 수정

--- frontend/src/pages/Home/style.js
+++ frontend/src/pages/Home/style.js
@@ -8,3 +8,39 @@ export const Container = styled.div`
 
   margin-top: 25px;
 `;
+
+export const BoxItem = styled.div`
+  display: flex;
+  gap: 5px;
+  justify-content: center;
+  align-items: center;
+  margin: 10px;
+`;
+
+export const Height = styled.div`
+  font-weight: bold;
+  font-size: 24px;
+`;
+
+export const Weight = styled.div`
+  font-weight: bold;
+  font-size: 24px;
+
+  margin-left: 10px;
+`;
+
+export const Intake = styled.div`
+  font-weight: bold;
+  font-size: 24px;
+  margin-right: 5px;
+`;
+
+export const TotalTime = styled.div`
+  font-weight: bold;
+  font-size: 24px;
+`;
+
+export const Description = styled.div`
+  color: var(--color-manatee);
+  text-align: center;
+`;
