73bf4101f0390677f9fce810924900de50e7b45b
ju0-0913 <juyeong_0913@naver.com>
2023-02-08 06:14:56 +0900

    findFoodByDate() 추가 : 날짜로 음식 찾기 찾기

--- backend/src/main/java/com/inti/nosily_coach/domain/food/repository/FoodRepositoryCustom.java
+++ backend/src/main/java/com/inti/nosily_coach/domain/food/repository/FoodRepositoryCustom.java
@@ -3,6 +3,7 @@ package com.inti.nosily_coach.domain.food.repository;
 import com.inti.nosily_coach.domain.food.model.Food;
 import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,4 +12,5 @@ public interface FoodRepositoryCustom {
     Optional<Food> findByName(String name); // 음식 이름으로 Food 객체를 찾는 메서드
     List<Food> findAll(); // 모든 음식 리스트를 다 반환하는 메서드
     List<Food> pageFood(Long memberId, Pageable pageable);
+    Food findFoodByDate(Long memberId, Long foodId, LocalDate localDate);
 }
