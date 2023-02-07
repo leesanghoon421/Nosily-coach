c61896ae4704bb1c11ba27c4bb99e616751d962f
LeeSumin <sumin4101@naver.com>
2023-02-07 15:26:24 +0900

    전반적인 틀 디자인

--- frontend/src/pages/Routine/style.js
+++ frontend/src/pages/Routine/style.js
@@ -1,39 +1,61 @@
 import styled from "styled-components";
 
 export const Pointblock = styled.div`
-  background-color: #EB455F;
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-start;
+  padding-bottom: 16px;
+  padding-left: 16px;
+
+  width: 360px;
+  height: 128px;
+
+  left: 0px;
+  top: 0px;
+  background: #1e90ff;
+  text-align: left;
+  font-size: 26px;
   color: white;
-  width: 50vw;
-  padding: 0.2em 0;
-  padding-left: 1em;
+  font-family: "Spoqa Han Sans Neo";
   
-  //border: 1px solid black;
-  //text-align: center;
-  display: black;
-  margin: 0 auto;
-  margin-top: 30px;
-  
+`;
+export const RoutineContainer = styled.div`
+ position: absolute;
+width: 320px;
+height: 363px;
+left: 20px;
+top: 315px;
+
+background: #FFFFFF;
+box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+border-radius: 10px;
+font-family: "Spoqa Han Sans Neo";
 `;
 
-export const RoutineContainer = styled.div`
- //background-color: #fcffe7;
-  width: 30vw;
-  border: 1px solid black;
-  
-  display: block;
-  margin: 0 auto;
-  margin-top: 30px;
-  
-`;
 
 export const DateContainer = styled.div`
-   display: flex;
-   flex-direction: row;
-   
-   justify-content: space-evenly;
-   text-align: center;
-   font-size: 20px;
-   align-items: center;
-   width: 60vw;
-   margin: 30px auto;
+  display: flex;
+  flex-direction: row;
+  
+  justify-content: center;
+  align-items: center;
+  width: 131px;
+  height: 25px;
+  left: 116px;
+  top: 287px;
+  margin: 0 auto;
+  margin-top: 1em;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 25px;
+`;
+export const NextButton = styled.button`
+  background-color: white;
+  border : white;
+  padding-left: 10px; 
+  font-size: 20px; 
+  cursor: pointer;
+  margin-right: 10px;
+  margin-left: 10px;
 `;
