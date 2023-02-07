7716b9c56887dfe736350775995a3c4ef77186e0
ju0-0913 <juyeong_0913@naver.com>
2023-02-07 21:49:08 +0900

    Eat 리포지터리 추가(찾기, 페이징처리)

--- backend/src/main/java/com/inti/nosily_coach/domain/Eat/repository/EatRepositoryCustomImpl.java
+++ backend/src/main/java/com/inti/nosily_coach/domain/Eat/repository/EatRepositoryCustomImpl.java
@@ -1,4 +1,40 @@
 package com.inti.nosily_coach.domain.Eat.repository;
 
-public class EatRepositoryCustomImpl implements EatRepositoryCustom{
+import com.inti.nosily_coach.domain.Eat.model.Eat;
+import com.inti.nosily_coach.domain.food.model.Food;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static com.inti.nosily_coach.domain.Eat.model.QEat.eat;
+
+@RequiredArgsConstructor
+public class EatRepositoryCustomImpl implements EatRepositoryCustom {
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    @Transactional
+    public Eat findEatByfoodName(Long memberId, Food food, Long DietRecordId) {
+        return jpaQueryFactory.selectFrom(eat)
+                .where(
+                        eat.dietRecord.member.id.eq(memberId),
+                        eat.dietRecord.id.eq(DietRecordId),
+                        eat.food.name.eq(food.getName())
+                ).fetchOne();
+    }
+
+    @Override
+    @Transactional
+    public List<Eat> pageEat(Long foodId, Pageable pageable) {
+        return jpaQueryFactory.selectFrom(eat)
+                .where(
+                        eat.food.id.eq(foodId)
+                ).orderBy(eat.createdAt.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+    }
 }
