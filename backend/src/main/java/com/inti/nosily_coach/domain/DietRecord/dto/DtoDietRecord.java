adb073774be9ab92ea3a5891ad4c9d7646a4d3fd
ju0-0913 <juyeong_0913@naver.com>
2023-02-08 02:18:33 +0900

    DietRecord 디티오 수정 : 음식들을 담는 eats의 타입 변경(List<Eat> -> List<DtoEat>)

--- backend/src/main/java/com/inti/nosily_coach/domain/DietRecord/dto/DtoDietRecord.java
+++ backend/src/main/java/com/inti/nosily_coach/domain/DietRecord/dto/DtoDietRecord.java
@@ -1,6 +1,6 @@
 package com.inti.nosily_coach.domain.DietRecord.dto;
 
-import com.inti.nosily_coach.domain.Eat.model.Eat;
+import com.inti.nosily_coach.domain.Eat.dto.DtoEat;
 import lombok.*;
 import java.util.List;
 
@@ -11,9 +11,9 @@ public class DtoDietRecord {
     private Long DietRecordId; // 식단기록 id
     private String createAt; // 생성날짜
     private String memo; // 메모
-    private List<Eat> eats; // 식단기록 리스트
+    private List<DtoEat> eats; // 식단기록 리스트
 
-    public static DtoDietRecord of(Long DietRecordId, String createAt, String memo, List<Eat> eats) {
+    public static DtoDietRecord of(Long DietRecordId, String createAt, String memo, List<DtoEat> eats) {
         return new DtoDietRecord(DietRecordId, createAt, memo, eats);
     }
 }
