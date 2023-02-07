c3d2a83b8234f479ab2187816ac48f16ae6d1d88
ju0-0913 <juyeong_0913@naver.com>
2023-02-08 06:17:12 +0900

    getFoodInfoForAIResponse() 추가 : AI에게 칼로리, 단백질, 탄수화물, 지방 데이터 넘겨주기

--- backend/src/main/java/com/inti/nosily_coach/domain/food/Service/FoodService.java
+++ backend/src/main/java/com/inti/nosily_coach/domain/food/Service/FoodService.java
@@ -1,9 +1,11 @@
 package com.inti.nosily_coach.domain.food.Service;
 
 import com.inti.nosily_coach.domain.food.dto.CreateFoodRequest;
+import com.inti.nosily_coach.domain.food.dto.GetFoodInfoForAIResponse;
 import com.inti.nosily_coach.domain.food.model.Food;
 import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,4 +14,5 @@ public interface FoodService {
     Long createFood(Long memberId, CreateFoodRequest createFoodRequest);
     Optional<Food> findOne(String food_name);
     List<Food> viewFood(Long memberId, Pageable pageable);
+    GetFoodInfoForAIResponse getFoodInfoForAIResponse(Long memberId, Long foodId, LocalDate localDate);
 }
--- backend/src/main/java/com/inti/nosily_coach/domain/food/Service/FoodServiceImpl.java
+++ backend/src/main/java/com/inti/nosily_coach/domain/food/Service/FoodServiceImpl.java
@@ -1,23 +1,32 @@
 package com.inti.nosily_coach.domain.food.Service;
 
-import com.inti.nosily_coach.auth.repository.MemberRepository;
+import com.inti.nosily_coach.auth.repository.OAuthRepository;
+import com.inti.nosily_coach.domain.DietRecord.model.DietRecord;
 import com.inti.nosily_coach.domain.Member.model.Member;
 import com.inti.nosily_coach.domain.food.dto.CreateFoodRequest;
+import com.inti.nosily_coach.domain.food.dto.GetFoodInfoForAIResponse;
 import com.inti.nosily_coach.domain.food.model.Food;
 import com.inti.nosily_coach.domain.food.repository.FoodRepository;
+import com.querydsl.core.types.Ops;
+import com.querydsl.core.types.dsl.NumberOperation;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
+
+import static com.inti.nosily_coach.domain.food.model.QFood.food;
+import static com.querydsl.core.types.dsl.Expressions.numberOperation;
 
 @Service
 @RequiredArgsConstructor
 public class FoodServiceImpl implements FoodService {
     private final FoodRepository foodRepository;
-    private final MemberRepository memberRepository;
+    private final OAuthRepository memberRepository;
+    private final JPAQueryFactory jpaQueryFactory;
 
     @Override
     public Optional<Food> findOne(String food_name) { // 전체 음식 리스트 중 하나의 음식을 조회하는 메서드
@@ -34,6 +43,12 @@ public class FoodServiceImpl implements FoodService {
     @Override
     public List<Food> viewFood(Long memberId, Pageable pageable) {
         return foodRepository.pageFood(memberId, pageable);
+    }
+
+    @Override
+    public GetFoodInfoForAIResponse getFoodInfoForAIResponse(Long memberId, Long foodId, LocalDate localDate) {
+        Food food = foodRepository.findFoodByDate(memberId, foodId, localDate);
+        return GetFoodInfoForAIResponse.of(food.getKcal(), food.getProtein(), food.getCar(), food.getFat());
     }
 
 }
