64dfe5c35d5ddbc4ce38a0bb245fd3a673710b69
Yeonhan Jung <yt4307@gmail.com>
2023-02-08 03:01:17 +0900

    [Feat] 운동 루틴 페이지 추가

--- frontend/src/components/RoutineBox/index.js
+++ frontend/src/components/RoutineBox/index.js
@@ -1,26 +1,17 @@
 import { useState } from "react";
-import * as styled from "./style.js";
-import { Link } from "react-router-dom";
+import * as styled from "./style";
 
 const RoutineBox = ({ part }) => {
-  
-  const [한세트갯수, set한세트갯수] = useState(0);
-  const [한세트시간, set한세트시간] = useState(0);
-  const [목표무게, set목표무게] = useState(0);
-  const [휴식시간, set휴식시간] = useState(0);
+  const [반복횟수, set반복횟수] = useState(0);
+  const [세트개수, set세트개수] = useState(0);
 
   return (
-    <>
-      {/* <Link to="/routineset"> */}
-        {/* 버튼을 클릭하면 routineset 페이지로 이동 */}
-        <styled.RoutineBox>
-          <div>{part}</div>
-          <div>{한세트갯수},{한세트시간}</div>
-          <div>{목표무게}</div>
-          <div>{휴식시간}</div>
-        </styled.RoutineBox>
-      {/* </Link> */}
-    </>
+    <styled.RoutineBox>
+      <div>{part}</div>
+      <styled.ReferralCount>
+        추천 횟수 : {반복횟수}회 {세트개수}세트
+      </styled.ReferralCount>
+    </styled.RoutineBox>
   );
 };
 export default RoutineBox;
---
+++ frontend/src/pages/Exercise/Routine/index.js
@@ -0,0 +1,38 @@
+import { useState } from "react"; // react의 useState 함수 사용
+import { Link, useLocation, Outlet } from "react-router-dom"; // react-router-dom의 useLocation, Outlet 함수 사용
+
+import * as styled from "./style"; // styled.js 파일에서 정의한 스타일들 가져오기
+import SubTitle from "components/SubTitle";
+import RoutineBox from "components/RoutineBox";
+
+const parts = ["가볍게 즐기는 유산소 루틴", "상체 1시간 불태우기"];
+
+const Routine = ({ setSubCategory }) => {
+  const [Date, SetDate] = useState("2023/2/7");
+  const location = useLocation(); //현재 URL을 가져온다
+  const currentUrl = location.pathname;
+
+  setSubCategory("추천 운동 루틴");
+
+  return (
+    <styled.Container>
+      {currentUrl !== "/exercise/routine" ? (
+        <Outlet />
+      ) : (
+        <div>
+          <SubTitle>추천 루틴 목록</SubTitle>
+          <styled.RoutineContainer>
+            {parts.map(element => (
+              <styled.BoxContents to="info">
+                <img src="img/icon.png" alt="icon" />
+                {element}
+              </styled.BoxContents>
+            ))}
+          </styled.RoutineContainer>
+        </div>
+      )}
+    </styled.Container>
+  );
+};
+
+export default Routine;
