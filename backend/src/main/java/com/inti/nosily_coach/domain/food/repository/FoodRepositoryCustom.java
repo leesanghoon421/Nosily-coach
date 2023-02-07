17b4b3c01e2c7943dc9d37285a625fa0a6b1d5c3
ju0-0913 <juyeong_0913@naver.com>
2023-02-07 21:50:44 +0900

    Food 리포지터리 추가(저장, 찾기, 페이징처리)

--- backend/src/main/java/com/inti/nosily_coach/domain/food/repository/FoodRepositoryCustom.java
+++ backend/src/main/java/com/inti/nosily_coach/domain/food/repository/FoodRepositoryCustom.java
@@ -1,4 +1,14 @@
 package com.inti.nosily_coach.domain.food.repository;
 
+import com.inti.nosily_coach.domain.food.model.Food;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface FoodRepositoryCustom {
+    Food saveFood(Food food); // 새로운 음식을 DB에 추가하는 메서드
+    Optional<Food> findByName(String name); // 음식 이름으로 Food 객체를 찾는 메서드
+    List<Food> findAll(); // 모든 음식 리스트를 다 반환하는 메서드
+    List<Food> pageFood(Long memberId, Pageable pageable);
 }
--- backend/src/main/java/com/inti/nosily_coach/domain/food/repository/FoodRepositoryCustomImpl.java
+++ backend/src/main/java/com/inti/nosily_coach/domain/food/repository/FoodRepositoryCustomImpl.java
@@ -1,4 +1,43 @@
 package com.inti.nosily_coach.domain.food.repository;
 
-public class FoodRepositoryCustomImpl implements FoodRepositoryCustom{
+import com.inti.nosily_coach.domain.food.model.Food;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+
+import java.util.*;
+
+import static com.inti.nosily_coach.domain.food.model.QFood.food;
+
+@RequiredArgsConstructor
+public class FoodRepositoryCustomImpl implements FoodRepositoryCustom {
+    private static Map<Long, Food> store = new HashMap<>(); // 음식 임시저장소
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Food saveFood(Food food) {
+        store.put(food.getId(), Food.newFood(food.getName(), food.getKcal(), food.getProtein(), food.getCar(), food.getFat()));
+        return food;
+    }
+
+    @Override
+    public Optional<Food> findByName(String name) {
+        return store.values().stream() // store에 저장된 Food 객체로 새로운 리스트를 만들어서 stream 객체로 바꿈.
+                .filter(food -> food.getName().equals(name)) // food 스트림의 첫번재 요소부터 마지막 요소까지 반복하면서 매개변수 name과 비교해 같으면 true를 반환하여 또다른 새로운 스트림 객체를 만들고 false면 버림.
+                .findAny(); // 그렇게 만들어진 새로운 스트림 객체에서 가장 첫번째 Food 객체를 Optional<Food>에 넣어서 반환하고, 빈 스트림 객체라면 null 값을 Optional<Food>에 넣어서 반환함.
+    }
+
+    @Override
+    public List<Food> findAll() {
+        return new ArrayList<>(store.values()); // store에 있는 Food 객체들로 새로운 ArrayList를 만들어 반환함.
+    }
+
+    @Override
+    public List<Food> pageFood(Long memberId, Pageable pageable) {
+        return queryFactory.selectFrom(food)
+                .limit(pageable.getPageSize())
+                .offset(pageable.getOffset())
+                .fetch();
+    }
 }
