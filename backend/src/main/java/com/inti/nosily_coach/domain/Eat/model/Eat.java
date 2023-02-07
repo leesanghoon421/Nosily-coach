9eb6f108312365e4cd0c213a4f02c841e5ac0116
ju0-0913 <juyeong_0913@naver.com>
2023-02-07 21:46:02 +0900

    Eat 엔티티 추가

--- backend/src/main/java/com/inti/nosily_coach/domain/Eat/model/Eat.java
+++ backend/src/main/java/com/inti/nosily_coach/domain/Eat/model/Eat.java
@@ -3,10 +3,7 @@ package com.inti.nosily_coach.domain.Eat.model;
 import com.inti.nosily_coach.domain.DietRecord.model.DietRecord;
 import com.inti.nosily_coach.domain.common.BaseEntity;
 import com.inti.nosily_coach.domain.food.model.Food;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,21 +15,37 @@ import lombok.NoArgsConstructor;
 public class Eat extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "dietRecord_id")
-    private DietRecord dietRecord;
+    private DietRecord dietRecord; // 식단기록 id
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "food_id")
-    private Food food;
+    private Food food; // 음식 id
+
+    @Column(nullable = false)
+    private String time; // 아침, 점심, 저녁, 간식
+
+    @Column
+    private Long intake; // 섭취량
 
     @Builder(access = AccessLevel.PRIVATE)
-    private Eat(DietRecord dietRecord, Food food) {
+    private Eat(DietRecord dietRecord, Food food, String time, Long intake) {
         this.dietRecord = dietRecord;
         this.food = food;
+        this.time = time;
+        this.intake = intake;
     }
 
-    public static Eat newEat(DietRecord dietRecord, Food food) {
+    public static Eat newEat(DietRecord dietRecord, Food food, String time, Long intake) {
         return Eat.builder()
                 .dietRecord(dietRecord)
-                .food(food).build();
+                .food(food)
+                .time(time)
+                .intake(intake)
+                .build();
+    }
+
+    public void updateEat(String time, Long intake) { // 업데이트하는 메서드
+        this.time = time;
+        this.intake = intake;
     }
 }
